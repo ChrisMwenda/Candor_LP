35e5a9d032407638cf0b86e06249d25690687215
Christopher <chrismbogo97@gmail.com>
2024-02-27 15:12:18 +0300

    form submission logic

--- src/components/AppAppBar.js
+++ src/components/AppAppBar.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import Logo from "./Assets/CandorHR.png"
 
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
@@ -84,9 +85,7 @@ function AppAppBar({ mode, toggleColorMode }) {
               }}
             >
               <img
-                src={
-                  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
-                }
+                src={Logo}
                 style={logoStyle}
                 alt="logo of sitemark"
               />
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,18 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
 export default function Hero() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => alert("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
   return (
     <Box
       id="hero"
@@ -30,89 +42,99 @@ export default function Hero() {
           pb: { xs: 8, sm: 12 },
         }}
       >
-        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
-          <Typography variant="h5" textAlign="center" color="text.secondary">
-            Hiring Managers, Recruiters, and Interviewers{" "}
-          </Typography>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit={handleSubmit}
+          sx={{ width: { xs: "100%", sm: "70%" } }}
+        >
+          <Stack spacing={2} useFlexGap>
+            <Typography variant="h5" textAlign="center" color="text.secondary">
+              Hiring Managers, Recruiters, and Interviewers{" "}
+            </Typography>
 
-          <Typography
-            component="h1"
-            variant="h1"
-            sx={{
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              alignSelf: "center",
-              textAlign: "center",
-            }}
-          >
-            Don't be a&nbsp;
             <Typography
-              component="span"
+              component="h1"
               variant="h1"
               sx={{
-                color: (theme) =>
-                  theme.palette.mode === "light"
-                    ? "primary.main"
-                    : "primary.light",
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                alignSelf: "center",
+                textAlign: "center",
               }}
             >
-              Jerk
+              Don't be a&nbsp;
+              <Typography
+                component="span"
+                variant="h1"
+                sx={{
+                  color: (theme) =>
+                    theme.palette.mode === "light"
+                      ? "primary.main"
+                      : "primary.light",
+                }}
+              >
+                Jerk
+              </Typography>
             </Typography>
-          </Typography>
-          <Typography variant="body1" textAlign="center" color="text.secondary">
-            We get it...You're{" "}
-            <span style={{ fontWeight: "bold" }}>
-              {" "}
-              sorting through 100s of applications, racing against deadlines,
-              and competing for the best{" "}
-            </span>
-            . But those applicants you left hanging? They don't see it that way.
-            They're{" "}
-            <span style={{ fontWeight: "bold" }}>
-              anxious, frustrated and probably think your company is a bunch of
-              jerks.{" "}
-            </span>{" "}
-            Even worse, they'll tell their friends about it.
-          </Typography>
-          <Typography variant="body1" textAlign="center" color="text.secondary">
-            We help you generate thoughtful, personalized feedback for every
-            applicant within minutes.
-          </Typography>
+            <Typography variant="body1" textAlign="center" color="text.secondary">
+              We get it...You're{" "}
+              <span style={{ fontWeight: "bold" }}>
+                {" "}
+                sorting through 100s of applications, racing against deadlines,
+                and competing for the best{" "}
+              </span>
+              . But those applicants you left hanging? They don't see it that way.
+              They're{" "}
+              <span style={{ fontWeight: "bold" }}>
+                anxious, frustrated and probably think your company is a bunch of
+                jerks.{" "}
+              </span>{" "}
+              Even worse, they'll tell their friends about it.
+            </Typography>
+            <Typography variant="body1" textAlign="center" color="text.secondary">
+              We help you generate thoughtful, personalized feedback for every
+              applicant within minutes.
+            </Typography>
 
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            alignSelf="center"
-            spacing={1}
-            useFlexGap
-            sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
-          >
-            <TextField
-              id="outlined-basic"
-              hiddenLabel
-              size="small"
-              variant="outlined"
-              aria-label="Enter your email address"
-              placeholder="Your email address"
-              inputProps={{
-                autocomplete: "off",
-                ariaLabel: "Enter your email address",
-              }}
-            />
-            <Button variant="contained" color="primary">
-              Start now
-            </Button>
+            <Stack
+              direction={{ xs: "column", sm: "row" }}
+              alignSelf="center"
+              spacing={1}
+              useFlexGap
+              sx={{ pt: 2 }}
+            >
+              <TextField
+                id="outlined-basic"
+                hiddenLabel
+                size="small"
+                variant="outlined"
+                aria-label="Enter your email address"
+                placeholder="Your email address"
+                inputProps={{
+                  autoComplete: "off",
+                  ariaLabel: "Enter your email address",
+                  name: "email", // Add name attribute for form submission
+                }}
+              />
+              <input type="hidden" name="form-name" value="contact" />
+              <Button type="submit" variant="contained" color="primary">
+                Start now
+              </Button>
+            </Stack>
+            <Typography
+              variant="caption"
+              textAlign="center"
+              sx={{ opacity: 0.8 }}
+            >
+              By clicking &quot;Start now&quot; you agree to our&nbsp;
+              <Link href="#" color="primary">
+                Terms & Conditions
+              </Link>
+            </Typography>
           </Stack>
-          <Typography
-            variant="caption"
-            textAlign="center"
-            sx={{ opacity: 0.8 }}
-          >
-            By clicking &quot;Start now&quot; you agree to our&nbsp;
-            <Link href="#" color="primary">
-              Terms & Conditions
-            </Link>
-          </Typography>
-        </Stack>
+        </form>
       </Container>
     </Box>
   );
